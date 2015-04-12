3b6f1f999f66e0bedbd9b2df7df9ff062e680157
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-04-12 06:01:46 -0300

    melhorado exibicao de erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,6 @@ class ApplicationController < ActionController::Base
   # @todo melhorar autenticação
   before_filter :authenticate
 
-  rescue_from ActionController::RoutingError, with: :render_404
-
-  rescue_from Exception do |exception|
-    render_500(exception)
-  end
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
@@ -20,35 +15,6 @@ class ApplicationController < ActionController::Base
       end
       @is_user != []
   	end
-  end
-
-  def render_404
-    puts "passei aqui :D"
-    respond_to do |format|
-      format.xml  { render :xml =>
-             "<error>Servidor falhou.</error>",
-             :status => 500 }
-      format.html { render :file => "#{Rails.root.join('public')}/500.html.erb", layout: "", :status => 500}
-      format.json { render :json =>
-              {:error => "Servidor falhou."}.to_json,
-               :status => 500 }
-    end and return
-  end
-
-def render_500(exception)
-    @exception = exception
-    debugger
-    puts exception
-    puts exception.backtrace.join("\n")
-    respond_to do |format|
-      format.xml  { render :xml =>
-             "<error>Servidor falhou.</error>",
-             :status => 500 }
-      format.html { render :file => "#{Rails.root.join('public')}/500.html.erb", layout: "", :status => 500}
-      format.json { render :json =>
-              {:error => "Servidor falhou."}.to_json,
-               :status => 500 }
-    end and return
   end
 
 end
