aa16c1c3cea143e753707db53082e9d047de22bf
Ariel Santos Moraes <ariel.s.moraes@gmail.com>
2015-04-12 02:31:02 -0300

    Alterando resposta json para a exibição de todos pets

--- app/controllers/pet_controller.rb
+++ app/controllers/pet_controller.rb
@@ -9,10 +9,37 @@ class PetController < ApplicationController
 	end
 
 	def get_all
-		@pets = Pet.all
-		@pets = {type: "pets", pets: @pets}
+		
+		@json_response = { 
+			type: "pets",
+			pets: []
+		}
 
-		render json: @pets
+		Pet.all.each do |pet|
+			@user = User.find(pet.user_id)
+			
+			@json_response[:pets] << {
+				nome: pet.nome,
+				genero: pet.sexo,
+				especie: pet.especie,
+				idade: pet.idade,
+				cor: pet.cor,
+				porte: pet.porte,
+				localizacao: "Porto Alegre",
+				foto: pet.foto,
+				descricao_longa: pet.descricao_longa,
+				legenda: pet.legenda,
+				protetor: {
+					id: @user.id,
+					nome: @user.name
+				}
+			}
+			
+		end
+
+		#@pets = {type: "pets", pets: @pets}
+
+		render json: @json_response
 	end
 
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,21 @@
 class Pet < ActiveRecord::Base
+  
+	@protetor
+
+	def initialize
+		@protetor = 'a'
+	end
+
+	def protetor
+		return @protetor
+	end
+
+	def protetor=(value)
+		@protetor = value
+	end
+
   belongs_to :user
-
   has_one :user
-
   has_many :likes
   has_many :users, through: :likes
 end
