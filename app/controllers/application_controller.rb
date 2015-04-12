dd0bbc8351e2a191323894832bf642808ea7b848
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-04-12 01:35:54 -0300

    adicionado previa de basic http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+
+  # @todo melhorar autenticação
+  before_filter :authenticate
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      @is_user = User.where({email:username, password: password})
+      @is_user != []
+    end
+  end
+
 end
