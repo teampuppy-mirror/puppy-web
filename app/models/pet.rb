1810a16b4bd750450b184e9e68a6e79579670c2f
Ariel Santos Moraes <ariel.s.moraes@gmail.com>
2015-04-12 03:57:19 -0300

    Adicionando contagem de likes na estrutura do json do pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -30,6 +30,31 @@ class Pet < ActiveRecord::Base
 		return @json_responde
 	end
 
+	def json_of_a_user(id)
+		@pet = Pet.find(id)
+		@user = User.find(@pet.user_id)
+
+		@json_responde = {
+			nome: @pet.nome,
+			genero: @pet.sexo,
+			especie: @pet.especie,
+			idade: @pet.idade,
+			cor: @pet.cor,
+			porte: @pet.porte,
+			localizacao: "Porto Alegre",
+			foto: @pet.foto,
+			descricao_longa: @pet.descricao_longa,
+			legenda: @pet.legenda,
+			likes: @pet.likes.count,
+			protetor: {
+				id: @user.id,
+				nome: @user.name
+			}
+		}
+
+		return @json_responde
+	end
+
   belongs_to :user
   has_one :user
   has_many :likes
