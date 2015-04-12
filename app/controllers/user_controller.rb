afb259125d117fc342cd419098fffaeecc75ef63
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-04-12 02:39:20 -0300

    user.post: criado metodo para criar usuario novo

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,19 @@
 class UserController < ApplicationController
 	skip_before_filter :verify_authenticity_token
-  
+
+  def create
+		@ja_existe = User.where({email:params[:email]})
+		if @ja_existe != []
+			render json: {code: 403, error: "Usuário já existe"}
+		else
+			# testar se dados sao validos. Sério
+
+			User.create(user_params)
+			render json: {code: 200, message: "Usuário criado com sucesso"}
+		end 
+  end
+
+
 	def get_active
 		@id = request.headers["HTTP_DEBUG_USER_ID"].to_i
 		@user = User.find(@id)
@@ -11,6 +24,12 @@ class UserController < ApplicationController
   def update
 		#@id = request.headers["HTTP_DEBUG_USER_ID"].to_i
 		#@user = User.find(@id)
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :name, :password)
   end
   
 end
